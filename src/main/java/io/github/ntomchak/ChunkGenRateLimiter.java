1664ed5a2f048c7fc80a70eed59e3c285316e9fc
ntomchak <55006670+ntomchak@users.noreply.github.com>
2020-02-16 10:55:12 -1000

    remove target and unnecessary interface

--- src/main/java/io/github/ntomchak/ChunkGenRateLimiter.java
+++ src/main/java/io/github/ntomchak/ChunkGenRateLimiter.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 
-public class ChunkGenRateLimiter extends JavaPlugin implements Listener {
+public class ChunkGenRateLimiter extends JavaPlugin {
 
 	private static ChunkGenRateLimiter instance;
 
